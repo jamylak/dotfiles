216974599133b0e4a50b4a737e7e0f0f106d34fa
jamylak <jamylak@gmail.com>
2025-09-08 12:23:58 +1000

    compile script usage

--- applescripts/compile_script.sh
+++ applescripts/compile_script.sh
@@ -1,5 +1,7 @@
 # Note: Could make it so PlistBuddy sets identifier
 # so everything is not called applet
+# Usage:
+# ./compile_script.sh toggle_apple_music.applescript toggle_apple_music.app
 SCRIPT="$1"
 APP="$2"
 set -euo pipefail
