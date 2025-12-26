7a273ce4abbc84271597132bef613b83a737cb01
jamylak <jamylak@gmail.com>
2025-12-26 16:36:21 +1100

    `c-i` `fish` binding

--- fish/config.fish
+++ fish/config.fish
@@ -990,4 +990,5 @@ end
 starship init fish | source
 set -gx FZF_DEFAULT_OPTS "--pointer=🔥 --layout=reverse --info=inline --height=80% --bind=ctrl-j:accept"
 fzf_configure_bindings --directory=\ct
+bind -M insert ctrl-i _fzf_search_directory
 source ~/.config/fish/config.local.post.fish
