a4f0ff310d459bce15306dcce97a09e1466ba189
jamylak <jamylak@gmail.com>
2025-12-29 15:53:48 +1100

    `tmux_fzf` on `a-p`

--- fish/config.fish
+++ fish/config.fish
@@ -100,9 +100,9 @@ bind -M insert \en 'commandline -r "nvim_nproj"; commandline -f execute'
 bind -M insert \ev 'commandline -r "nvim ." ; commandline -f execute'
 # bind -M insert \ev "nvim"
 bind -M insert \eq "commandline --function kill-whole-line"
-bind -M insert \ep 'commandline -r "nvim_join_fzf"; commandline -f execute'
 # bind -M insert \cp 'commandline -r "tmux_fzf"; commandline -f execute'
 # bind -M insert \em 'commandline -r "tmux_session_fzf"; commandline -f execute'
+bind -M insert \ep 'commandline -r "tmux_fzf"; commandline -f execute'
 bind -M insert \co "__smart_cd_or_insert_path; commandline --function repaint"
 bind --mode insert \cy 'commandline --insert (pbpaste)'
 bind -M insert \em fish_clipboard_paste
