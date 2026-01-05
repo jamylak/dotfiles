1af53134c5eb1f0b04cbf191940cebe5bbcf8e7c
jamylak <jamylak@gmail.com>
2026-01-05 21:06:37 +1100

    `gkb` -> `git commit -b`

--- fish/config.fish
+++ fish/config.fish
@@ -141,12 +141,12 @@ abbr -a gfo "git_fetch_main; git_checkout_origin"
 abbr -a gko git_checkout_origin
 abbr -a gkt "git checkout (tv git-branch)"
 abbr -a gt "git checkout (tv git-branch)"
-abbr -a gkb "git checkout (tv git-branch)"
 abbr -a N nvim -c ":Neogit"
 abbr -a ng nvim -c ":Neogit"
 abbr -a gi "git init"
 abbr -a gb "git branch"
 abbr -a gk git checkout
+abbr -a gkb "git checkout -b"
 abbr -a gm --set-cursor=! "git commit -m \"!\""
 # gh extension install gennaro-tedesco/gh-s
 abbr -a ghc --set-cursor=! "git clone (gh s \"!\") && cd (ls -t | head -n 1)"
