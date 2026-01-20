383e443ac483c01d6fb2e6f2e91a1e2fe6618112
jamylak <jamylak@gmail.com>
2026-01-20 21:13:59 +1100

    no for loops in `fzf_proj_paths.sh`

--- scripts/fzf_proj_paths.sh
+++ scripts/fzf_proj_paths.sh
@@ -1,31 +1,17 @@
 #!/usr/bin/env fish
 
-set -l search_paths
-
-for p in /tmp ~ ~/.local/share ~/.local/share/nvim/lazy ~/.config ~/bar ~/proj
-    if test -e $p
-        set search_paths $search_paths $p
-    end
-end
+set -l search_paths /tmp ~ ~/.local/share ~/.local/share/nvim/lazy ~/.config ~/bar ~/proj
 
 if test (uname) = Darwin
-    for p in /Applications ~/.Trash ~/Desktop ~/Downloads
-        if test -e $p
-            set search_paths $search_paths $p
-        end
-    end
+    set search_paths $search_paths /Applications ~/.Trash ~/Desktop ~/Downloads
 end
 
 if test -e /etc/NIXOS; and test -e ~/nixconf
     set search_paths $search_paths ~/nixconf
 end
 
-for p in ~/bar/* ~/proj/* ~/.config/dotfiles* ~/.config/nvim*
-    if test -e $p
-        set search_paths $search_paths $p
-    end
-end
+set search_paths $search_paths ~/bar/* ~/proj/* ~/.config/dotfiles* ~/.config/nvim*
 
 if test (count $search_paths) -gt 0
-    ls -dt $search_paths
+    ls -dt $search_paths 2>/dev/null
 end
